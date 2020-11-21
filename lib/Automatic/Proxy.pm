9ce514fb5f2a74f4f299aa5d8b77adf5a4bc4dba
Edoardo Mantovani <Baseband@cpan.org>
2020-11-21 09:27:53 +0100

    first beta

---
+++ lib/Automatic/Proxy.pm
@@ -0,0 +1,45 @@
+# Made by Edoardo Mantovani, 2020
+# scrape automatic proxies
+
+package Automatic::Proxy;
+
+use strict;
+no strict 'refs';
+use warnings;
+
+use LWP::UserAgent;
+
+# interface for hidemy.name
+
+use constant HIDEMYNAME => "https://hidemy.name/en/proxy-list/?";
+
+my $lwp = LWP::UserAgent->new();
+
+my %anonimity_levels = {
+  'High' => 4,
+  'Average' => 3,
+  'Low' => 2,
+  'No' => 1,
+};
+
+my %proxy_types = {
+  'Http' => "h",
+  'Https' => "s",
+  'Socks4' => 4,
+  'Socks5' => 5,
+};
+
+sub scrape_hidemy{
+  my( $port, $type, $anonimity ) = @_;
+  if( $port > 65535 || undef( $port ) ){
+    die "not inserted port variable or maximun port number is wrong!\n";
+  }
+  
+  my $final_url  = "https://hidemy.name/en/proxy-list/?ports=" . $port . "&type=" . $proxy_types{$type} . "&anon=" .  $anonimity_levels{$anonimity} . "#list";
+  $lwp->get( $final_url );
+
+
+}
+
+
+1;
